29dacc77e66adf72c99d30940f7e1ebe7aafdd78
angelicacvo <108093638+angelicacvo@users.noreply.github.com>
2025-06-16 21:49:41 -0500

    Initial commit

---
+++ ex3.js
@@ -0,0 +1,9 @@
+let shelterPetQuantity = 143;
+let housePetQuantity = 2;
+
+console.log(shelterPetQuantity + housePetQuantity);
+
+let firstName = "Angelica";
+let lastName = "Cuervo";
+
+console.log(firstName + " " + lastName);
---
+++ ex4.js
@@ -0,0 +1,8 @@
+let age = prompt("Enter your age ");
+age = parseInt(age)
+
+if (age >= 18){
+    alert("You can pass")
+}else {
+    alert("You're under 18")
+}
